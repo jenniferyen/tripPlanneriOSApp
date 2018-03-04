da6ae4b81e3ef553d4fad6af3acc41f896142616
Jennifer Yen <jenniferyen@Eugenes-MacBook-Air.local>
2018-03-03 21:58:22 -0500

    initial commit

---
+++ Apoio/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Apoio
+//
+//  Created by Jennifer Yen on 3/3/18.
+//  Copyright © 2018 jenyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //MARK: Properties
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK: Actions
+    @IBOutlet weak var setButton: UIButton!
+}
+
